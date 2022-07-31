7636f1ff542f6dd50acecbeaa6067806f8d16fd5
Pashalazarev <mymessagebox9@gmail.com>
2022-07-31 22:04:43 +0300

    1)Main storyboard - done 2) func startButtonPressed() not finished

--- TraficLightApp/ViewController.swift
+++ TraficLightApp/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLightView: UIView!
+    
+    @IBOutlet var yellowLightView: UIView!
+    
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var starButton: UIButton!
+    
+    @IBOutlet var nextButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        
+        redLightView.layer.cornerRadius = 59.5
+        yellowLightView.layer.cornerRadius = 59.5
+        greenLightView.layer.cornerRadius = 59.5
+        
+        starButton.layer.cornerRadius = 15
+    }
+    
+    @IBAction func startButtonPressed() {
+        starButton.setTitle("Next", for: .normal)
+        
+    }
     }
 
 
-}
+
 
